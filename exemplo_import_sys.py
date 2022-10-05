74443b68e26818bd97fe60bf6ed485c35acaef07
root <root@DESKTOP-KTPGB03.localdomain>
2022-10-05 14:33:04 -0300

    import.sys

---
+++ exemplo_import_sys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+Nome = sys.argv[1] # get first command line parameter
+Cor = sys.argv[2] # get second command line parameter
+Atividade = sys.argv[3] # get third command line parameter
+# now print a message to the screen
+print('meu nome é',Nome)
+print('minha cor favorita é',Cor)
+print('minha atividade favorita é',Atividade)
