acd4d49904be356fde1f9f6ce783407cba593e39
root <root@DESKTOP-KTPGB03.localdomain>
2022-11-02 16:54:29 -0300

    exercicio lista1

---
+++ lista1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+lista = ["um"," dois"," tres", "quatro"," cinco"]
+
+print(lista)
+
+print(lista[2])
+
+lista[2] = "vinte"
+
+print(lista)
+
+lista.append("treze")
+
+print(lista)
+
+lista.insert(0,"bacon")
+
+print(lista)
+
+lista.pop(0)
+
+lista.pop()
+
+print(lista)
+
+lista1 =  "Y0".join(lista)
+
+print(lista1)
