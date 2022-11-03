6d9c546fd5105c6e9b84142173d36c33f0e0bbc1
root <root@DESKTOP-KTPGB03.localdomain>
2022-11-02 21:24:21 -0300

    ex de for 1

---
+++ loop_for.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+sequencia = [101,2,15,22,95,33,2,27,72,15,52]
+sequencia.sort()
+
+for x in sequencia :
+	if x%2 != 0:
+		continue
+	print(x)
+
+
+for x in range(1,20):
+	print(x)
